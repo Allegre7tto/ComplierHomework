e2f2303288a5116f0d9e8c3559cc049c559a0137
Allegre7tto <allegre7tto@qq.com>
2025-06-13 00:15:31 +0800

    remove build

--- build/CMakeFiles/test_parser_target.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/test_parser_target"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/test_parser_target.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/allegre7tto/Coding/Compilers
-# Build directory: /Users/allegre7tto/Coding/Compilers/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
